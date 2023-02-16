08e960eb883083232164e989bbe90d86d8b96eb7
mykhailo.novko <mykhailo.novko@gamingcorps.com>
2023-02-17 01:09:45 +0200

    Lines added. Opacity added

--- src/index.js
+++ src/index.js
@@ -10,64 +10,127 @@ const resize = (canvas, window) => {
   return {width, height};
 }
 
-const create = (ctx, w, h) => {
-  return new Desk(ctx, w, h);
-}
-
 window.onload = () => {
   canvas = document.getElementById('canvas');
   ctx = canvas.getContext('2d');
 
   const {width, height} = resize(canvas, window);
 
-  desk = create(ctx, width, height);
-  desk.animate(0);
+  desk = new Desk(ctx, width, height);
 }
 
-window.addEventListener('resize', function () {
-  cancelAnimationFrame(animationFrame);
+// window.addEventListener("mousemove", (event) => {
+//   mouse.x = event.x;
+//   mouse.y = event.y;
+// })
 
-  const {width, height} = resize(canvas, window);
+class Particle {
+  #ctx;
+  #color;
+  #x_step = 0;
+  #y_step = 0;
+  #x_curr = 0;
+  #y_curr = 0;
+  #x_from = 0;
+  #y_from = 0;
 
-  desk = create(ctx, width, height);
-  desk.animate(0);
-});
+  opacity = 1;
 
-const mouse = {
-  x: 0,
-  y: 0,
-};
+  constructor(ctx, x_from, y_from, x_step, y_step, color) {
+    this.#ctx = ctx;
+    this.#ctx.fillStyle = 'white';
+    this.#ctx.strokeStyle = 'white';
+    this.#ctx.lineWidth = Math.random() * 10;
+    this.#ctx.moveTo(x_from, y_from);
 
-window.addEventListener("mousemove", (event) => {
-  mouse.x = event.x;
-  mouse.y = event.y;
-})
+    this.#x_step = Math.random() > .5 ? x_step : -x_step;
+    this.#y_step = Math.random() > .5 ? y_step : -y_step;
+
+    this.#x_curr = this.#x_from = x_from;
+    this.#y_curr = this.#y_from = y_from;
+
+    this.#ctx.stroke();
+    this.#ctx.fill();
+  }
+
+  update() {
+    //this.#ctx.save();
+    this.opacity = 1 - (this.getDistanceFromStart() / 300);
+    if (this.opacity <= 0)
+      this.opacity = 0;
+
+    this.#ctx.globalAlpha = this.opacity;
+
+    //this.#ctx.restore();
+
+    this.#ctx.moveTo(this.#x_curr, this.#y_curr);
+
+    this.#x_curr += this.#x_step;
+    this.#y_curr += this.#y_step;
+
+    this.#ctx.beginPath();
+    this.#ctx.arc(this.#x_curr, this.#y_curr, 12, 0, 2 * Math.PI, false);
+    this.#ctx.fillStyle = 'green';
+    this.#ctx.fill();
+  }
+
+  get position() {
+    return {
+      x: this.#x_curr,
+      y: this.#y_curr
+    };
+  }
+
+  getDistanceFromStart() {
+    return this.getDistanceFromPoint(this.#x_from, this.#y_from)
+  }
+
+  getDistanceFromPoint(xFrom, yFrom) {
+    const x1 = this.#x_curr - xFrom;
+    const y1 = this.#y_curr - yFrom;
+    return Math.sqrt(x1 * x1 + y1 * y1);
+  }
+}
 
 class Desk {
   #ctx;
   #width;
   #height;
+  #items = [];
 
   constructor(ctx, width, height) {
     this.#ctx = ctx;
     this.#width = width;
     this.#height = height;
-    this.#ctx.strokeStyle = 'white';
 
-    this.x = 0;
-    this.y = 0;
-    this.angle = 0;
+    document.addEventListener('mousemove', (event) => {
+      const ctx = this.#ctx;
+      const x_step = Math.random() * 1.2;
+      const y_step = Math.random() * 1.2;
+      const x_from = event.x;
+      const y_from = event.y;
 
-    this.lastTime = 0;
-    this.interval = 1000/60;
-    this.timer = 0;
-    this.d = 0;
+      const particle = new Particle(ctx, x_from, y_from, x_step, y_step,);
+      this.#items.push(particle);
+      if (this.#items.length === 1) {
+        particle.name = 'test1';
+      }
+      if (this.#items.length === 2) {
+        particle.name = 'test2';
+      }
+
+    })
+
+    this.animate();
   }
 
-  #drawLine(x, y, to_x, to_y) {
+  drawLine(x1, y1, x2, y2) {
+    this.#ctx.lineWidth = 1;
+    this.#ctx.strokeStyle = '#ffffff';
+
     this.#ctx.beginPath();
-    this.#ctx.moveTo(x, y);
-    this.#ctx.lineTo(to_x, to_y);
+    this.#ctx.moveTo(x1, y1);
+    this.#ctx.lineTo(x2, y2);
     this.#ctx.stroke();
   }
 
@@ -76,43 +139,32 @@ class Desk {
   }
 
   animate(timeStamp) {
-    const deltaTime = timeStamp - this.lastTime;
-    this.lastTime = timeStamp;
+    this.#clear(0, 0, this.#width, this.#height);
+    this.#ctx.beginPath();
 
-    if (this.timer > this.interval) {
-      this.d += 0.01;
-      this.#clear(0, 0, this.#width, this.#height);
+    this.#items.forEach((item, i) => {
+      this.#items.forEach((near, j) => {
+        const p1 = item.position;
+        const p2 = near.position;
+        const dist = item.getDistanceFromPoint(p2.x, p2.y);
 
-      // const x1 = (this.#width/2 + Math.sin(this.angle)*10) + mouse.x;
-      // const y1 = (this.#height/2 + Math.cos(this.angle)*10) + mouse.y;
-      const x1 = mouse.x;
-      const y1 = mouse.y;
-      // const x2 = this.#width/2;
-      // const y2 = this.#height/2;
-
-      let prevX = 0;
-      let prevY = 0;
-
-      const stepX = Math.abs(1 + (this.#width * (Math.sin(this.d))));
-      const stepY = Math.abs(1 + (this.#height * (Math.cos(this.d))));
-
-      console.log((Math.cos(this.d)));
-
-      for (let x = 0; x < this.#width; x += stepX) {
-        for (let y = 0; y < this.#height; y += stepY) {
-          const currX = x;
-          const currY = y;
-          this.#drawLine(prevX, prevY, currX, currY);
-          prevX = currX;
-          prevY = currY;
+        if (item.name === 'test1' && near.name === 'test2') {
+          console.log(dist)
         }
-      }
 
-      this.timer = 0;
-    } else {
-      this.timer += deltaTime;
+        if (dist < 100) {
+          this.drawLine(p1.x, p1.y, p2.x, p2.y);
+        }
+      })
+      item.update();
+    })
+
+    for (let i=this.#items.length-1; i>0; --i) {
+      if (this.#items[i].opacity <= 0.5) {
+        this.#items.splice(i,1);
+      }
     }
 
-    animationFrame = requestAnimationFrame(this.animate.bind(this))
+    requestAnimationFrame(this.animate.bind(this))
   }
 }
